a4d924d31e8aeb6105d3ea729c19f631ad88a229
LauraVenegas6 <aleja09venegas@gmail.com>
2025-10-10 15:02:12 -0500

    Agregar proyecto Spring Boot inicial con configuración Maven

---
+++ src/main/java/com/dosw/masterchef/MasterchefApiApplication.java
@@ -0,0 +1,13 @@
+package com.dosw.masterchef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterchefApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterchefApiApplication.class, args);
+	}
+
+}
