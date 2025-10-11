4821b0d67d5976afcb622c1325e4577e469d1a5b
LauraVenegas6 <aleja09venegas@gmail.com>
2025-10-11 17:14:27 -0500

    configuracion inicial Swagger-- API

--- src/main/java/com/dosw/masterchef/MasterchefApiApplication.java
+++ src/main/java/com/dosw/masterchef/MasterchefApiApplication.java
@@ -3,6 +3,11 @@ package com.dosw.masterchef;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * main class of the application
+ * @author DOSW Company -- Laura Alejandra Venegas
+ * @version 1.0
+ */
 @SpringBootApplication
 public class MasterchefApiApplication {
 
